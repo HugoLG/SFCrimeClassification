1dca3291a486a927d752b265f6376db504210c6b
EnriqueB <enriqueBonilla93@outlook.com>
2016-02-15 19:11:53 +0000

    Changes to preprocessing
    The method now modifies the category and the pdDistrict of the crime to be a number between 0 and the max number of each. The method also returns the dictionaries containing this information in case its needed for a method.

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,23 @@
 import numpy as np
-#from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans
 import csv
 
-def preprocessData():
+def preprocessData(k):
 	"""
 	This method opens a csv file and preporcess the information
+	Receives:
+	integer k: number of clusters with which to separate the addresses
+	
+	Returns
+	np.array Data: The order of the columns is the following
+		Category Day(Monday = 0) PdDistrict AddressCluster X Y Time Year Month DayNumber
+	dictionary Category: Contains the mapping of the categories to numbers
+	dictionary District: Contains the mapping of the districts to numbers
+	
 	potential changes:
 	1.- Provide it with the name of the file to open
-	2.- Provide it with more options, such as the ammount of clusters wanted
 	3.- Have it remove the description and resolution columns instead of doing it manually
 	5.- Fix the warning
-	6.- Change category into format (0,0,....1,0) if possible, or consider pair (like 'THEFT/LARCENY') as
-		a new category. This will allow using only one column as category 
 	7. More ideas?
 	"""
 	with open('train.csv','r') as dest_f:
@@ -20,14 +26,18 @@ def preprocessData():
 	#Gets rid of the header, could also be changed to receive a file with no headers
 	data = data[1:]
 	clusterData = [clusterData[5:7] for clusterData in data]
-	k=15
-        
+
         clusterData = np.array(clusterData)
 	kmeans = KMeans(n_clusters=k)
 	kmeans.fit(clusterData)
 	labels = kmeans.predict(clusterData)
-	#substitute the addresses for cluster labels
-	for i in range(0, len(data)):
+
+        counterCategory = 0
+        counterPdDistrict = 0
+        categoryDict = {}
+        pdDistrictDict = {}
+        
+        for i in range(0, len(data)):
                 if("Monday" in data[i][2]):
                     data[i][2] = 0
                 elif("Tuesday" in data[i][2]):
@@ -43,6 +53,16 @@ def preprocessData():
                 elif("Sunday" in data[i][2]):
                     data[i][2] = 6
 
+                if not categoryDict.has_key(data[i][1]):
+                    categoryDict[data[i][1]] = counterCategory
+                    counterCategory += 1
+                data[i][1] = categoryDict[data[i][1]]
+
+                if not pdDistrictDict.has_key(data[i][3]):
+                    pdDistrictDict[data[i][3]] = counterPdDistrict
+                    counterPdDistrict += 1
+                data[i][3] = pdDistrictDict[data[i][3]]
+                #substitute the addresses for cluster labels
 		data[i][4] = labels[i]
 		i+=1
 	
@@ -59,6 +79,10 @@ def preprocessData():
             data = np.concatenate((data, dates), axis = 1)
             data = np.delete(data, 0, 1)
 
-	return data
+	return data, categoryDict, pdDistrictDict
 
-prepData = preprocessData()
+prepData, catDict, districtDict = preprocessData(15)
+
+#print prepData[0]
+#print str(catDict)
+#print str(districtDict)
