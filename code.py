1aa240cddf7d25900994fd93e53a7dceaa1331b6
Aek MFA <Aek MFA>
2016-02-22 16:05:56 +0000

    generated training data. For this set, category values are only affected by DaysOfWeek

--- code.py
+++ code.py
@@ -1,56 +1,104 @@
 import numpy as np
 from sklearn.cluster import KMeans
 import csv
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn import tree
-from sklearn.cross_validation import train_test_split
-import warnings
-warnings.filterwarnings("ignore")
 
-def preprocessData():
+def preprocessData(k):
 	"""
 	This method opens a csv file and preporcess the information
+	Receives:
+	integer k: number of clusters with which to separate the addresses
+	
+	Returns
+	np.array Data: The order of the columns is the following
+		Category Day(Monday = 0) PdDistrict AddressCluster Time Year Month DayNumber
+	dictionary Category: Contains the mapping of the categories to numbers
+	dictionary District: Contains the mapping of the districts to numbers
+	
 	potential changes:
 	1.- Provide it with the name of the file to open
-	2.- Provide it with more options, such as the ammount of clusters wanted
 	3.- Have it remove the description and resolution columns instead of doing it manually
-	4.- Separate the timestamp into different fields (year, month, day of the month, time)
 	5.- Fix the warning
-	6.- More ideas?
+	7. More ideas?
 	"""
-	with open('D:\Essex\CE903 Group Project\Data\\train_cut.csv','r') as dest_f:
+	with open('train.csv','r') as dest_f:
 		data_iter = csv.reader(dest_f, delimiter = ',', quotechar = '"')
 		data = [data for data in data_iter]
 	#Gets rid of the header, could also be changed to receive a file with no headers
 	data = data[1:]
 	clusterData = [clusterData[5:7] for clusterData in data]
-	k=15
-	clusterData = np.array(clusterData)
+
+        clusterData = np.array(clusterData)
 	kmeans = KMeans(n_clusters=k)
 	kmeans.fit(clusterData)
 	labels = kmeans.predict(clusterData)
 
-	#substitute the addresses for cluster labels
-	for i in range(0, len(data)):
+        counterCategory = 0
+        counterPdDistrict = 0
+        categoryDict = {}
+        pdDistrictDict = {}
+        
+        for i in range(0, len(data)):
+                if("Monday" in data[i][2]):
+                    data[i][2] = 0
+                elif("Tuesday" in data[i][2]):
+                    data[i][2] = 1
+                elif("Wednesday" in data[i][2]):
+                    data[i][2] = 2
+                elif("Thursday" in data[i][2]):
+                    data[i][2] = 3
+                elif("Friday" in data[i][2]):
+                    data[i][2] = 4
+                elif("Saturday" in data[i][2]):
+                    data[i][2] = 5
+                elif("Sunday" in data[i][2]):
+                    data[i][2] = 6
+
+                if not categoryDict.has_key(data[i][1]):
+                    categoryDict[data[i][1]] = counterCategory
+                    counterCategory += 1
+                data[i][1] = categoryDict[data[i][1]]
+
+                if not pdDistrictDict.has_key(data[i][3]):
+                    pdDistrictDict[data[i][3]] = counterPdDistrict
+                    counterPdDistrict += 1
+                data[i][3] = pdDistrictDict[data[i][3]]
+                #substitute the addresses for cluster labels
 		data[i][4] = labels[i]
 		i+=1
-	data = np.array(data)
+	
+        data = np.delete(data, 5, 1);
+        data = np.delete(data, 5, 1);
+        data = np.array(data)
 
-	return data
+        if(":" in data[0][0] and "-" in data [0][0]):
+            #need to split dates
+            times = [(int(dates[0].split(" ")[1].split(":")[0])*60+int(dates[0].split(" ")[1].split(":")[1])) for dates in data]
+            dates = [dates[0].split(" ")[0].split("-") for dates in data]
+            times = np.array([times])
+            dates = np.array(dates)
+            data = np.concatenate((data, np.atleast_1d(times.T)), axis=1)
+            data = np.concatenate((data, dates), axis = 1)
+            data = np.delete(data, 0, 1)
 
-prepData = preprocessData()
+	return data, categoryDict, pdDistrictDict
+
+prepData, catDict, districtDict = preprocessData(15)
+
+print prepData[0]
+#print str(catDict)
+#print str(districtDict)
 dataset = prepData
 
 vectorizer = CountVectorizer()
-dataset = dataset.reshape(-1,7)
+#dataset = dataset.reshape(-1,7)
 #print dataset
 
-data = dataset[:,0]
+data = dataset[:,1]
 #print data
 for i in range(2,len(dataset[0])):
     data =np.column_stack((data,dataset[:,i]))
 #print "dataset only"
-print data
+#print data
 DictList = []
 
 def createDic(DictList,raw):
@@ -79,13 +127,10 @@ def createDic(DictList,raw):
     return newList
 
 data = createDic(DictList,data)
-target = dataset[:,1]
+target = dataset[:,0]
 print("%d documents" % len(data))
 
 print
-
-
-        
 
 dic = dict()
 countCat = 0
@@ -102,7 +147,7 @@ for i in range(len(target)):
     Y[i] = dic[target[i]]
 #print target
 #print Y
-print("%d categories" % countCat)
+print("%d categories" % len(catDict))
 
 def QuickReduct(C,D): 
     gammaCD = 0
@@ -156,72 +201,58 @@ def trainTrees(numTrees, Xt, yt):
     slt = QuickReduct(DT,DecisionTable)
     return slt
     
-avgacc = 0.0
 times = 0
-avgpre = 0.0
-avgrec = 0.0
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.500000, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.100000, random_state=42)
 pool = trainTrees(11,X_train,y_train)
-correctly = 0.0
 
 confTable = [[[0 for x in range(2)]for x in range(2)]for x in range(countCat)]
     
 for index in range(len(y_test)):
-    countResult = [0 for x in range(countCat)]
+    countResult = [0 for x in range(len(catDict))]
     for t in pool:
         result = t.predict(X_test[index])
         countResult[result] = countResult[result]+1  
     maxIndex = countResult.index(max(countResult))
     ind = y_test[index]    
     if(y_test[index] == maxIndex):
-        correctly = correctly+1
-        ind = y_test[index]
-        confTable[ind][0][0] = confTable[ind][0][0]+1 #TP
+        confTable[ind][0][0] +=1 #TP
         for i in range(countCat):
             if i != ind:
-                confTable[i][1][1] = confTable[i][1][1]+1 #TN of else
+                confTable[i][1][1] += 1 #TN of else
     else:
-        confTable[ind][0][1] = confTable[ind][0][1]+1 #FP of index
-        confTable[maxIndex][1][0] = confTable[maxIndex][1][0]+1 #FN of maxIndex
+        confTable[ind][0][1] += 1 #FP of index
+        confTable[maxIndex][1][0] += 1 #FN of maxIndex
 
-    #print confTable
+print confTable
 TP,TN,FN,FP = 0,0,0,0
-
+acc_l, pre_l, rec_l = 0.0,0.0,0.0
+    #TP_l, TN_l, FN_l, FP_l = 0,0,0,0
 for index in range(len(confTable)):
-    TP = TP+confTable[index][0][0]
-    TN = TN+confTable[index][1][1]
-    FN = FN+confTable[index][1][0]
-    FP = FP+confTable[index][0][1]
-        #print TP, FP, FN, TN
+    TP_l = confTable[index][0][0]
+    TN_l = confTable[index][1][1]
+    FN_l = confTable[index][1][0]
+    FP_l = confTable[index][0][1]
         
-TP = (float)(TP)/(float)(len(X))
-FP = (float)(FP)/(float)(len(X))
-FN = (float)(FN)/(float)(len(X))
-TN = (float)(TN)/(float)(len(X))
+    if TP_l != 0 | FN_l !=0 | FP_l != 0:
+        times += 1
+    
+        TP = TP+TP_l
+        TN = TN+TN_l
+        FN = FN+FN_l
+        FP = FP+FP_l
+        #print TP, FP, FN, TN
+    #print correctly, len(y_test)
+print times
+TP = (float)(TP)/(float)(times)
+FP = (float)(FP)/(float)(times)
+FN = (float)(FN)/(float)(times)
+TN = (float)(TN)/(float)(times)
     
 precision = TP/(TP+FP)
 recall = TP/(TP+FN)
+    
 print ("precision %f" %(precision))
 print ("recall %f" %(recall))
     
-avgpre = avgpre+precision
-avgrec = avgrec+recall
-avgacc = avgacc + correctly/len(y_test)
-times = times+1
-print ("accuracy %f" %(correctly/len(y_test)))
-"""
-avg = avgacc/times
-avgrec = avgrec/times
-avgpre = avgpre/times
 
-print ("baseline acc avg: %f" % avg)
-print ("baseline precision avg: %f" %avgpre)
-print ("baseline recall: %f" %avgrec)
-  
-clf = tree.DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
-    sc = clf.score(X_test,y_test)
-    print ("test %f" %sc)
-    print
-"""
-    
+print ("accuracy %f" %((TP+TN)/(TP+FP+FN+TN)))
